fc7cde9b9204013338ce7df5770e3c7d5e2366d9
yangsoosong <song.yang@husky.neu.edu>
2018-09-12 16:46:25 -0400

    Add Dog class for sample

---
+++ ConsoleApp/ConsoleApp/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public partial class Dog
+    {
+        // properties hold values
+        public string Name { get; set; }
+        propfull
+
+
+        // only by this class
+        private void Foo() { }
+
+        // only this and derived classes
+        protected void Bar() { }
+
+        // only in this assembly
+        internal void Bar() { }
+    }
+
+    class Poodle : Dog
+    {
+        void x() { this.Bar}
+    }
+}
