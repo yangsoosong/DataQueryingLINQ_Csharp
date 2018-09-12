4c037043e8427ceacf9cebdf1178ee0b9db1669c
yangsoosong <song.yang@husky.neu.edu>
2018-09-12 16:50:40 -0400

    Fixed dog class

--- ConsoleApp/ConsoleApp/Dog.cs
+++ ConsoleApp/ConsoleApp/Dog.cs
@@ -6,12 +6,67 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+
+    //void Breakfast()
+    //{
+    //    var bird = new Animal<Egg>();
+    //    var pig = new Animal<Piglet>();
+    //}
+    public class Animal<T> where T : Offspring
+    {
+        public T Offspring { get; set; }
+    }
+
+    public abstract class Offspring { }
+    public class Egg : Offspring { }
+    public class Piglet : Offspring { }
+
+    public class Trainer
+    {
+        void Operate()
+        {
+            var dog = new Poodle();
+            dog.HasSpoken += Dog_HasSpoken;
+        }
+
+        private void Dog_HasSpoken(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
     public partial class Dog
     {
         // properties hold values
         public string Name { get; set; }
-        propfull
+        private int myVar;
 
+        public int MyProperty
+        {
+            get { return myVar; }
+            private set { myVar = value; }
+        }
+
+        public event EventHandler HasSpoken;
+
+        // Set default value for string input variable
+        public void Speak(string what = "bark")
+        {
+            // TODO
+            if (HasSpoken != null)
+                HasSpoken(this, EventArgs.Empty);
+        }
+
+        public void Speak(int times, string what = "bark")
+        {
+            // TODO
+        }
+
+        public void Speak(int times, string what = "bark", Boolean sit = true)
+        {
+
+        }
 
         // only by this class
         private void Foo() { }
@@ -20,11 +75,14 @@ namespace ConsoleApp
         protected void Bar() { }
 
         // only in this assembly
-        internal void Bar() { }
+        internal void Bar2() { }
     }
 
     class Poodle : Dog
     {
-        void x() { this.Bar}
+        void x()
+        {
+            this.Speak(2, sit:true);
+        }
     }
 }
