cc4a6179b55a9af9987b0c201d9349878be5106f
yangsoosong <song.yang@husky.neu.edu>
2018-09-18 15:03:45 -0400

    Add Stack

---
+++ ConsoleApp/ConsoleApp/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Stack
+    {
+        Entry top;
+
+        public void push(object data)
+        {
+            top = new Entry(top, data);
+        }
+
+        public object pop()
+        {
+            if (top == null) throw new InvalidOperationException();
+            object result = top.data;
+            top = top.next;
+            return result;
+        }
+
+        class Entry
+        {
+            public Entry next;
+            public object data;
+
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/StackTest.cs
@@ -0,0 +1,20 @@
+﻿using Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Test
+{
+    static void Main()
+    {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(10);
+        s.push(100);
+        Console.WriteLine(s.pop());
+        Console.WriteLine(s.pop());
+        Console.WriteLine(s.pop());
+    }
+}
